3a8eda033b8bdfe5f63407420c4bf05557b41af2
Kayo Osako <osakok@cardiff.ac.uk>
2025-05-22 00:55:16 +0100

    Remove build directory from repo

--- build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_TodoList_FILE C:/Users/c24089518/Documents/GitRepo/TodoList/TodoList/build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/TodoList.exe)
-set(__QT_DEPLOY_TARGET_TodoList_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_TodoList_RUNTIME_DLLS C:/Qt/6.9.0/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.9.0/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.9.0/mingw_64/bin/Qt6Core.dll)
